bac15a04bcffeed6039b6680554d951268121832
mesodiar <mils.meso@gmail.com>
2016-12-27 20:34:02 +0700

    "Save into database to show various kitten"

--- kitten/models.py
+++ kitten/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Size(models.Model):
+class Kitten_image(models.Model):
+    image_name = models.CharField(max_length=100)
     width = models.CharField(max_length=10)
     height = models.CharField(max_length=10)
--- kitten/views.py
+++ kitten/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 from .forms import KittenForm
+from .models import Kitten_image
+from datetime import datetime
 import requests
 
 
@@ -10,13 +13,33 @@ def kitten_home(request):
 
         if form.is_valid():
             print "mils"
+            obj = Kitten_image()
+            title = str(datetime.now())
             url = "http://placekitten.com/g/" + form.cleaned_data['input_width'] + "/" + form.cleaned_data['input_height']
             kitten = requests.get(url)
-            f = open("kittens.png", "w")
+            f = open( title + ".png", "w")
             f.write(kitten.content)
             f.close
+            obj.image_name = title + ".png"
+            obj.width = form.cleaned_data['input_width']
+            obj.height = form.cleaned_data['input_height']
+            obj.save()
+
     else:
         form = KittenForm()
 
+    all_image = Kitten_image.objects.all()
+
+    x = ""
+    for index in range(len(all_image)):
+        if index % 4 == 0:
+            x += '<div class="row">'+'<div class="col-xs-6 col-sm-3"><img src="' + settings.STATIC_URL + all_image[index].image_name + '"></div>'
+        elif index % 4 == 3:
+            x += '<div class="col-xs-6 col-sm-3"><img src="' + settings.STATIC_URL + all_image[index].image_name + '"></div>'+ '</div>'
+        else:
+            x += '<div class="col-xs-6 col-sm-3"><img src="' + settings.STATIC_URL + all_image[index].image_name + '"></div>'
+    x += '</div>'
+
+
     print form.errors
-    return render(request, 'kitten_home.html', {'kitten_image': "kittens.png"})
+    return render(request, 'kitten_home.html', {'kitten_image': all_image, 'n' : x})
